41e6996b5e3ecd8d23827a360823d1f06d3e29c7
deepanshudangi <deepanshudangi10@gmail.com>
2021-06-03 19:19:42 +0530

    added date function

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 //jshint esversion:6
 
 const express = require("express");
+const router = express.Router()
 const bodyParser = require("body-parser");
 
 const app = express();
-
+app.set('view engine', 'ejs');
+// app.use(app.router);
+// routes.initialize(app);
 app.get("/", function (req, res) {
-    res.send("Hello");
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", { kindofday: day });
+
 });
 
 app.listen(3000, function () {
